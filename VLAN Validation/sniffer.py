76fd4fc8792df2bff971c58e34568efaf8b2fb2c
Suriya491 <141767334+Suriya491@users.noreply.github.com>
2026-07-08 10:50:02 +0530

    Delete VLAN Validation/sniffer.py

--- VLAN Validation/sniffer.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import sniff, wrpcap
-
-def capture(interface: str, timeout: int = 5):
-    packets = sniff(iface=interface, timeout=timeout)
-    wrpcap("capture.pcap", packets)
-    print(f"Captured {len(packets)} packets.")
-    return packets
